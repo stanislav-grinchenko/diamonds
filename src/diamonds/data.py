5f01ec3f8d5f1a224cc71cd08d54834036cf0c07
Stanislav Grinchenko <stasagr@gmail.com>
2026-03-09 17:24:18 +0100

    create and train model

--- src/diamonds/data.py
+++ src/diamonds/data.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import seaborn as sns
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
 # Import other necessary libraries here
 
+def keep_not_null(row) :
+    """keep_not_null is a function that checks if there are any null values in the row. 
+    If there are, it returns False, otherwise it returns True.
+
+    Args:
+        row represents a row of the dataframe
+
+    Returns:
+        bool: True if there are no null values in the row, False otherwise
+    """
+    if 0 in row.values : return False
+    return True
 
 def load_data(cache = True) -> pd.DataFrame:
     """
@@ -16,7 +34,7 @@ def load_data(cache = True) -> pd.DataFrame:
     pd.DataFrame
         The diamonds dataset
     """
-    pass
+    return sns.load_dataset('diamonds')
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -32,9 +50,9 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         The cleaned diamonds dataset
     """
-    pass
+    return df[df.apply(keep_not_null,axis=1)]
 
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_data(df: pd.DataFrame, preprocsessor: Pipeline) -> pd.DataFrame:
     """
     Preprocess the diamonds dataset.
 
@@ -48,9 +66,25 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         The preprocessed diamonds dataset
     """
-    pass
+    df_cat = df.select_dtypes(include="category")
+    cat_pipe = Pipeline(
+        [ ("cat_imp",SimpleImputer(strategy="most_frequent"))
+        ,("ohe",OneHotEncoder(drop="first",sparse_output=False))
+        ])
+    num_pipe = Pipeline(
+        [("knn_imp", KNNImputer(n_neighbors=5))
+        ,("scaler", StandardScaler())
+        ])
+    preprocessor = ColumnTransformer(
+        [("numeric",num_pipe, make_column_selector(dtype_include="number"))
+        ,("categorical", cat_pipe, make_column_selector(dtype_exclude="number"))
+        ]).set_output(transform="pandas")
+    
+    
+    
+    
 
-def create_X_y(df: pd.DataFrame) ->tuple[pd.DataFrame, pd.Series]:
+def create_train_test_X_y(df: pd.DataFrame, test_size: float, random_state: int) ->tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
     """
     Create the feature matrix X and target vector y from the diamonds dataset.
 
@@ -61,15 +95,19 @@ def create_X_y(df: pd.DataFrame) ->tuple[pd.DataFrame, pd.Series]:
 
     Returns
     -------
-    (pd.DataFrame, pd.Series)
+    (pd.DataFrame, pd.Series, pd.DataFrame, pd.Series)
         The feature matrix X and target vector y
     """
-    pass
+    X = df.drop(columns=["price"])
+    y = df["price"]
+    X_train, X_test, y_train, y_test  = train_test_split(X,y, test_size=test_size, random_state=random_state)
+    return X_train, y_train, X_test, y_test
 
 
 
 if __name__ == "__main__":
     df = load_data()
-    # df_clean = clean_data(df)
+    df_clean = clean_data(df)
+    X_train, y_train, X_test, y_test  = create_train_test_X_y(df_clean)
     # df_preprocessed = preprocess_data(df_clean)
-    # X, y = create_X_y(df_preprocessed)
+    
--- src/diamonds/registry.py
+++ src/diamonds/registry.py
@@ -1,13 +1,17 @@
 
 from sklearn.base import BaseEstimator
 from diamonds.params import MODEL_REGISTRY 
+import pickle
+import os
 
 def save_model(model, path):
     """Save the model to the specified path."""
-    # Implement the logic to save the model (e.g., using pickle, joblib, etc.)
-    pass
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
 
 def load_model(path) -> BaseEstimator:
     """Load the model from the specified path."""
     # Implement the logic to load the model (e.g., using pickle, joblib, etc.)
-    pass
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    return model
